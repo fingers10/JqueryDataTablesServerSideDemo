c0dbe66356f33d38255e8b50d7589f9eeed84249
Abdul Rahman <43729469+fingers10@users.noreply.github.com>
2019-01-18 15:33:35 +0530

    Search

---
+++ ServerSideMultiColumnSortingAndSearching/Models/DefaultSearchExpressionProvider.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace ServerSideMultiColumnSortingAndSearching.Models
+{
+    public class DefaultSearchExpressionProvider : ISearchExpressionProvider
+    {
+        protected const string EqualsOperator = "eq";
+
+        public virtual IEnumerable<string> GetOperators()
+        {
+            yield return EqualsOperator;
+        }
+
+        public virtual ConstantExpression GetValue(string input)
+            => Expression.Constant(input);
+
+        public virtual Expression GetComparison(MemberExpression left, string op, ConstantExpression right)
+        {
+            switch (op.ToLower())
+            {
+                case EqualsOperator: return Expression.Equal(left, right);
+                default: throw new ArgumentException($"Invalid Operator '{op}'.");
+            }
+        }
+    }
+}
