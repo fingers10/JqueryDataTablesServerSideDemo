daa43f7d41d3f0acd0e5a259399d7b746c3c4331
Abdul Rahman <43729469+fingers10@users.noreply.github.com>
2019-01-18 15:35:53 +0530

    Search

---
+++ ServerSideMultiColumnSortingAndSearching/Models/SearchableDateTimeAttribute.cs
@@ -0,0 +1,14 @@
+using Aetna.Informatics.DARTHWeb.Infrastructure;
+using System;
+
+namespace ServerSideMultiColumnSortingAndSearching.Models
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class SearchableDateTimeAttribute : SearchableAttribute
+    {
+        public SearchableDateTimeAttribute()
+        {
+            ExpressionProvider = new DateTimeSearchExpressionProvider();
+        }
+    }
+}
