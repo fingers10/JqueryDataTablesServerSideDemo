2eb75b80344838925eca7391d724eaf9851c6e55
Abdul Rahman <abdulrahman.smsi@gmail.com>
2019-01-19 22:10:43 +0530

    Added datatables library

--- ServerSideMultiColumnSortingAndSearching/Startup.cs
+++ ServerSideMultiColumnSortingAndSearching/Startup.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ServerSideMultiColumnSortingAndSearching
@@ -15,6 +10,7 @@ namespace ServerSideMultiColumnSortingAndSearching
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +21,7 @@ namespace ServerSideMultiColumnSortingAndSearching
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) => {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
