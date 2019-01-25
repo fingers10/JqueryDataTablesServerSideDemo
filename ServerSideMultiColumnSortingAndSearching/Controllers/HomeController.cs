11bee6787e1d64af071148426c9dbfc47d6cc606
Abdul Rahman <43729469+fingers10@users.noreply.github.com>
2019-01-25 20:27:19 +0530

    Added Load Table Controller Logic

--- ServerSideMultiColumnSortingAndSearching/Controllers/HomeController.cs
+++ ServerSideMultiColumnSortingAndSearching/Controllers/HomeController.cs
@@ -8,5 +8,28 @@ namespace ServerSideMultiColumnSortingAndSearching.Controllers
         {
             return View();
         }
+        
+        [HttpPost]
+        public IActionResult LoadTable([FromBody]DTParameters<ClassName> param)
+        {
+            try
+            {
+                //var memberInfos = await _databaseService.GetMemberInfoAsync(Environment, param.StringValue1);
+                //param.DataSource = new MemberClaimInfoDAO().GetMemberClaimInfo(Environment, await GetWarehouseIdStringAsync(memberInfos), param.StringValue2);
+                //param.ApplySearchAndSort<MemberClaimInfoDataModel>();
+
+                return new JsonResult(new DTResult<MemberClaimInfoBEO>
+                {
+                    draw = param.Draw,
+                    data = param.DataSource,
+                    recordsFiltered = param.DataSource.Count,
+                    recordsTotal = param.DataSource.Count
+                });
+            }
+            catch (Exception e)
+            {
+                return new JsonResult(new { error = "Internal Server Error" });
+            }
+        }
     }
 }
