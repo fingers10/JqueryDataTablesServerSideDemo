0985f090862e889c4788f893ffbb98e81fe34d10
Abdul Rahman <abdulrahman.smsi@gmail.com>
2019-01-27 19:57:11 +0530

    bug fixes

--- ServerSideMultiColumnSortingAndSearching/Infrastructure/SearchableIntAttribute.cs
+++ ServerSideMultiColumnSortingAndSearching/Infrastructure/SearchableIntAttribute.cs
@@ -7,7 +7,7 @@ namespace ServerSideMultiColumnSortingAndSearching.Infrastructure
     {
         public SearchableIntAttribute()
         {
-            ExpressionProvider = new LongSearchExpressionProvider();
+            ExpressionProvider = new IntSearchExpressionProvider();
         }
     }
 }
